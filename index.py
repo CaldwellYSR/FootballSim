4c7742e08841e60ec1ed46ace0dfc303c3c60ce7
Matthew Caldwel <caldwellysr@gmail.com>
2015-10-13 19:10:20 -0400

    added notes from yesterday and changed some naming around

--- index.py
+++ index.py
@@ -3,24 +3,24 @@
 import StackFSM as FSM
 import time
 
-class Ant(object):
+class Game(object):
 
-  brain = None
+  fsm = None
   cnt = 0
 
   def __init__(self):
 
     self.cnt = 0
-    self.brain = FSM.StackFSM()
-    self.brain.pushState(self.findLeaf)
+    self.fsm = FSM.StackFSM()
+    self.fsm.pushState(self.findLeaf)
 
   def findLeaf(self):
     self.cnt += 1
     if(self.cnt == 5):
       self.cnt = 0
       print("Found Leaf... going home")
-      self.brain.popState()
-      self.brain.pushState(self.goHome)
+      self.fsm.popState()
+      self.fsm.pushState(self.goHome)
     else:
       print("Looking for Leaf")
 
@@ -29,16 +29,16 @@ class Ant(object):
     if(self.cnt == 5):
       self.cnt = 0
       print("Leaf is home... looking for more")
-      self.brain.popState()
-      self.brain.pushState(self.findLeaf)
+      self.fsm.popState()
+      self.fsm.pushState(self.findLeaf)
     else:
       print("Going home...")
 
   def update(self, dt):
-    self.brain.update()
+    self.fsm.update()
 
 if __name__ == "__main__":
-  ant = Ant()
+  game = Game()
   lastFrameTime = 0
   FPS = 1
   while True:
@@ -50,4 +50,4 @@ if __name__ == "__main__":
     if sleepTime > 0:
       time.sleep(sleepTime)
 
-    ant.update(dt)
+    game.update(dt)
